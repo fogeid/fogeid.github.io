280a4c176010edebac9df77417f452bae1c64508
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2020-03-22 12:19:36 -0400

    Site migrado para VueJS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
-$('nav a').click(function(e){
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			menuHeight = $('nav').innerHeight(),
-			targetOffset = $(id).offset().top;
-	$('html, body').animate({
-		scrollTop: targetOffset - 140
-	}, 500);
+// $('nav a').click(function(e){
+// 	e.preventDefault();
+// 	var id = $(this).attr('href'),
+// 			menuHeight = $('nav').innerHeight(),
+// 			targetOffset = $(id).offset().top;
+// 	$('html, body').animate({
+// 		scrollTop: targetOffset - 140
+// 	}, 500);
+// });
+
+let app = new Vue({
+	el: '#app',
+	data: {
+		name: 'Diego Batista',
+		course: 'Computer Science',
+		university: 'Universidade Federal do Mato Grosso',
+		linkUniversity: 'https://www.ufmt.br/ufmt/site/',
+		work: 'Estuda.com',
+		linkWork: 'https://estuda.com/',
+		github: 'Github',
+		linkGithub: 'https://github.com/fogeid',
+		linkedin: 'Linkedin',
+		linkLinkedin: 'https://www.linkedin.com/in/dbatista/',
+		img: 'img/foto.jpg',
+	}
 });
