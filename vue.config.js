0014acdc0af6064c52e51a1cfab1c364acf83851
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2020-07-22 01:12:14 -0400

    update

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pluginOptions: {
+    quasar: {
+      importStrategy: 'kebab',
+      rtlSupport: false
+    }
+  },
+  transpileDependencies: [
+    'quasar'
+  ],
+  publicPath: 'fogeid.github.io'
+}
